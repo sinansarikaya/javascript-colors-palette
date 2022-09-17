ef47ff3a8126bdb96c97b4590de0d05ada122608
Sinan <sinan.sarykaya@gmail.com>
2022-09-17 10:11:43 +0200

    localstorage setItem

--- js/app.js
+++ js/app.js
@@ -3,8 +3,18 @@ const hamburger = document.querySelector(".hamburger");
 const colors = document.querySelectorAll("#colors .palette");
 const colorCode = document.querySelectorAll(".color-code");
 const lock = document.querySelectorAll(".lock");
+const copy = document.querySelectorAll(".fa-copy");
+const save = document.querySelector(".save");
+const saveBox = document.querySelector("#saveBox");
+const paletteName = document.querySelector("#palette-name");
+const saveSubmit = document.querySelector(".saveSubmit");
+const pName = document.querySelector(".pName");
+const colorList = document.querySelector(".colorList");
 
 let isLocked = [false, false, false, false, false];
+let boxOpen = false;
+
+const alert = (type, msg) => {};
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -16,12 +26,13 @@ const changeColor = () => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     if (isLocked[i] === false) {
       colors[i].style.backgroundColor = `#${randomColor}`;
-      colorCode[i].innerText = `#${randomColor}`;
+      colorCode[i].innerText = `${randomColor}`;
     }
   }
 };
+
 document.addEventListener("keydown", (e) => {
-  if (e.code === "Space") {
+  if (e.code === "Space" && !boxOpen) {
     changeColor();
   }
 });
@@ -39,8 +50,40 @@ lock.forEach((lockEl, i) =>
       event.add("fa-lock-open");
       isLocked[i] = false;
     }
-    //
   })
 );
 
+copy.forEach((copyEl, i) =>
+  copyEl.addEventListener("click", (e) => {
+    navigator.clipboard.writeText(
+      e.target.closest("div").querySelector(".color-code").innerText
+    );
+  })
+);
+
+save.addEventListener("click", () => {
+  saveBox.classList.add("active");
+  boxOpen = true;
+});
+
+saveSubmit.addEventListener("click", () => {
+  let palettes = {};
+  let palettesArray = [];
+  colorCode.forEach((colorCodeEl) => {
+    palettesArray.push(colorCodeEl.innerText);
+  });
+
+  palettes[paletteName.value] = palettesArray;
+  localStorage.setItem("Colors Palette", JSON.stringify(palettes));
+});
+
+getItems = () => {
+  let getList = localStorage.getItem("Colors Palette");
+
+  getList = JSON.parse(getList);
+  let colorKeys = String(Object.keys(getList));
+  console.log(colorKeys);
+  console.log(getList[colorKeys]);
+};
+getItems();
 changeColor();
